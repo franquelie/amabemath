e95aa3333d524fa50f76cefd8805011d2f6edafb
franquelie <franquelie@gmail.com>
2025-04-27 16:52:47 +0800

    Added background tilemap

---
+++ app/src/main/java/com/amabe/math/GameDisplay.java
@@ -0,0 +1,56 @@
+package com.amabe.math;
+
+import android.graphics.Rect;
+
+import com.amabe.math.gameobject.GameObject;
+
+public class GameDisplay {
+    public final Rect DISPLAY_RECT;
+    private int widthPixels;
+    private int heightPixels;
+    private GameObject centerObject;
+    private double displayCenterX;
+    private double displayCenterY;
+    private double gameToDisplayCoordinatesOffsetX;
+    private double gameToDisplayCoordinatesOffsetY;
+    private double gameCenterX;
+    private double gameCenterY;
+
+
+    public GameDisplay(int widthPixels, int heightPixels, GameObject centerObject) {
+        this.widthPixels = widthPixels;
+        this.heightPixels = heightPixels;
+        DISPLAY_RECT = new Rect(0, 0, widthPixels, heightPixels);
+
+        this.centerObject = centerObject;
+
+        displayCenterX = widthPixels/2.0;
+        displayCenterY = heightPixels/2.0;
+    }
+
+
+    public void update() {
+        gameCenterX = centerObject.getPositionX();
+        gameCenterY = centerObject.getPositionY();
+
+        gameToDisplayCoordinatesOffsetX = displayCenterX - gameCenterX;
+        gameToDisplayCoordinatesOffsetY = displayCenterY - gameCenterY;
+    }
+
+    public double gameToDisplayCoordinatesX(double x) {
+        return x + gameToDisplayCoordinatesOffsetX;
+    }
+
+    public double gameToDisplayCoordinatesY(double y) {
+        return y + gameToDisplayCoordinatesOffsetY;
+    }
+
+    public Rect getGameRect() {
+        return new Rect(
+                (int)(gameCenterX - widthPixels/2),
+                (int) (gameCenterY - heightPixels/2),
+                (int) (gameCenterX + widthPixels/2),
+                (int) (gameCenterY + heightPixels/2)
+        );
+    }
+}
